c86d04b0bd274c5e83116d87e66ae11e2276f87e
github <sagi.cohen37@gmail.com>
2020-08-26 16:18:47 +0300

    Context creation

---
+++ musicly/Data/musiclyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using musicly.Models;
+
+namespace musicly.Data
+{
+    public class musiclyContext : DbContext
+    {
+        public musiclyContext (DbContextOptions<musiclyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<musicly.Models.User> User { get; set; }
+    }
+}
