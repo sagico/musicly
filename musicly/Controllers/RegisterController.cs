510f60b54a912bc0080bf4a4214c24dae8a2b05e
Liorb <liorbalba@gmail.com>
2020-09-04 15:33:35 +0300

    Client Register added

---
+++ musicly/Controllers/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using musicly.Data;
+using musicly.Models;
+
+namespace musicly.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly musiclyContext _context;
+
+        public RegisterController(musiclyContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("FirstName,LastName,BirthDate,UserName,Password,City")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.IsAdmin = false;
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+    }
+}
