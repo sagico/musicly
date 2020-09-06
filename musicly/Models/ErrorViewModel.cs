3c7eea063d2f9a9a918e93b4a354d0b881ac4f8e
github <sagi.cohen37@gmail.com>
2020-09-06 20:32:16 +0300

    Error page edition

--- musicly/Models/ErrorViewModel.cs
+++ musicly/Models/ErrorViewModel.cs
@@ -4,8 +4,11 @@ namespace musicly.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        //public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        //public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string ErrorCode { get; set; }
+
+        public string Description { get; set; }
     }
 }
